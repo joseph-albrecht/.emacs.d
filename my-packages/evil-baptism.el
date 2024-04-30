43d0b2816ead11a56a52159726525b26bdbd337e
Joseph Albrecht <joey@joseph-albrecht.me>
2024-04-30 00:43:14 -0700

    keybindings for delete-other-windows and copy-window on -/+

--- my-packages/evil-baptism.el
+++ my-packages/evil-baptism.el
@@ -85,6 +85,8 @@
 (define-key evil-normal-state-map "I" 'evil-insert-line)
 (define-key evil-normal-state-map "0" 'evil-join)
 (define-key evil-normal-state-map "M" 'evil-set-marker)
+(define-key evil-normal-state-map "-" 'delete-other-windows )
+(define-key evil-normal-state-map "+" 'copy-window)
 
 (define-key evil-normal-state-map "o" 'evil-open-below)
 (define-key evil-normal-state-map "O" 'evil-open-above)
